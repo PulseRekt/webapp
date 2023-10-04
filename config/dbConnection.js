1e4d0ee19121e084cf31e37e93f9332da5fb765d
Barathi Sridhar <137120146+pulse-beep@users.noreply.github.com>
2023-10-03 22:04:20 -0400

    added env

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
-// Replace these values with your database information
-const sequelize = new Sequelize('cloud', 'root', 'Thenothing1!', {
-  host: 'localhost', 
-  port:3306,
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { DB_HOST, DB_PORT, DB_DATABASE, DB_USERNAME, DB_PASSWORD } = process.env;
+
+
+const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
+  host: DB_HOST, 
+  port:DB_PORT,
   dialect: 'mysql', 
 });
 
--- config/dbsConnection.js
+++ config/dbsConnection.js
@@ -1,12 +1,16 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { DB_HOST, DB_PORT, DB_DATABASE, DB_USERNAME, DB_PASSWORD } = process.env;
 
 // Replace these values with your database information
 const sequelize = new Sequelize({
   dialect: 'mysql', 
-  host: 'localhost',
-  port: 3306,
-  username: 'root',     
-  password: 'Thenothing1!',     
+  host: DB_HOST,
+  port: DB_PORT,
+  username: DB_USERNAME,     
+  password: DB_PASSWORD,     
 });
 
 export default sequelize;
