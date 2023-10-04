488c0b1f48da3307fbc1f91e718afa4953cfd6da
Barathi Sridhar <137120146+pulse-beep@users.noreply.github.com>
2023-10-03 20:01:46 -0400

    checking connection

--- config/dbsConnection.js
+++ config/dbsConnection.js
@@ -2,11 +2,11 @@ import { Sequelize } from "sequelize";
 
 // Replace these values with your database information
 const sequelize = new Sequelize({
-  dialect: 'mysql', // Specify your database dialect (e.g., mysql, postgres, sqlite, etc.)
+  dialect: 'mysql', 
   host: 'localhost',
   port: 3306,
-  username: 'root',     // Replace with your database username
-  password: 'Thenothing1!',     // Replace with your database password
+  username: 'root',     
+  password: 'Thenothing1!',     
 });
 
 export default sequelize;
