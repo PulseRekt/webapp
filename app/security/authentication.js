4d31c371d6b2b80a2740d99b960d9b70d8e16bde
Barathi Sridhar <114298089+barathis98@users.noreply.github.com>
2023-10-03 22:22:03 -0400

    Assignments (#35)
    * initial commit with the boilerplate
    
    * added integration test
    
    * added package json
    
    * fixed a dependency error
    
    * fixed a bug
    
    * added dependency
    
    * added mariadb installation
    
    * modified workflow
    
    * testing
    
    * added nodemon
    
    * added db automatic db creation
    
    * added db creation logic
    
    * checking connection
    
    * password
    
    * using mysql image
    
    * bug
    
    * adding root password
    
    * bug
    
    * added mysql docker
    
    * removed db check
    
    * bug 10
    
    * testing cicd
    
    * removed push
    
    * test
    
    * added env
    
    * added env to workflow
    
    * added forbidden response
    
    ---------
    
    Co-authored-by: Barathi Sridhar <137120146+pulse-beep@users.noreply.github.com>

--- app/security/authentication.js
+++ app/security/authentication.js
@@ -3,17 +3,13 @@ import bcrypt from 'bcrypt';
 
 const handleBasicAuthentication = async(headerString) => {
     if (headerString) {
-        // console.log(headerString);
       const base64Credentials = headerString.split(' ')[1];
       const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
       const [username, password] = credentials.split(':');
 
       const user = await findUserByNameAndPass(username);
       console.log("inside authentication");
-    //   console.log(user);
-    // console.log(user.id);
 
-    // console.log(user.id);
     try {
         const match = await bcrypt.compare(password, user.password);
     
@@ -22,7 +18,6 @@ const handleBasicAuthentication = async(headerString) => {
           return {
             authenticated: true,
             userId:user.id
-            // userId:user.
           };
         } else {
           console.log('Password is incorrect');
@@ -33,7 +28,6 @@ const handleBasicAuthentication = async(headerString) => {
         return { authenticated: false };
       }
     }
-    //   console.log(user);
     
     else{
         return {
