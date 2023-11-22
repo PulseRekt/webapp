e8030688cbb3107a1f7f036b644f9fee23e7260b
Barathi Sridhar <137120146+pulse-beep@users.noreply.github.com>
2023-11-22 14:39:36 -0500

    added sns

--- app/controller/submissionController.js
+++ app/controller/submissionController.js
@@ -3,6 +3,9 @@ import Submission from "../model/Submission.js";
 import { countAssignmentById, getAssignmentById } from "../service/assignmentService.js";
 import handleBasicAuthentication from '../security/authentication.js'
 import * as ss from '../service/submissonService.js'
+import { snsPublish } from "../utils/snsPublish.js";
+
+const arn = process.env.SNS_ARN;
 
 export const createSubmission = async(req,res,next)=>{
     try{
@@ -48,6 +51,8 @@ export const createSubmission = async(req,res,next)=>{
                 if (assignment.num_of_attempts > count){
 
                     if (assignment.deadline && new Date() <= new Date(assignment.deadline)) {
+
+                        snsPublish('testing');
                         await ss.createSubmission(Sub);
                         return res.status(201).send("Submission Accepted");
                     } else {
---
+++ app/utils/snsPublish.js
@@ -0,0 +1,25 @@
+import AWS from 'aws-sdk';
+
+
+export const snsPublish = (message)=>{
+
+    const arn = process.env.SNS_ARN
+
+    AWS.config.update({region:"us-east-1"});
+
+var params = {
+    Message: message,
+    TopicArn: arn
+  };
+
+  var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
+
+publishTextPromise.then(
+function(data) {
+console.log(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
+console.log("MessageID is " + data.MessageId);
+}).catch(
+function(err) {
+console.error(err, err.stack);
+});
+}
