6b789a06db364dcd71b535e2d8c5a43544e52380
Barathi Sridhar <137120146+pulse-beep@users.noreply.github.com>
2023-11-23 20:34:06 -0500

    added logs

--- app/utils/snsPublish.js
+++ app/utils/snsPublish.js
@@ -1,11 +1,12 @@
 import AWS from 'aws-sdk';
+import logger from '../../logger/logger';
 
 
 export const snsPublish = (message)=>{
 
     const arn = process.env.SNS_ARN
 
-    AWS.config.update({region:"us-east-1"});
+    // AWS.config.update({region:"us-east-1"});
 
 var params = {
     Message: message,
@@ -17,9 +18,12 @@ var params = {
 publishTextPromise.then(
 function(data) {
 console.log(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
+logger.info(`Message ${params.Message} sent to the topic ${params.TopicArn}`)
 console.log("MessageID is " + data.MessageId);
+logger.info("MessageID is " + data.MessageId)
 }).catch(
 function(err) {
 console.error(err, err.stack);
+logger.error(err)
 });
 }
